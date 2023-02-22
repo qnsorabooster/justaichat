0df54b51e43cf9e09127ab346f512268607ae6e2
qnsorabooster <124510198+qnsorabooster@users.noreply.github.com>
2023-02-22 16:35:24 +0000

    ooko

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,184 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  Alert,
+  ScrollView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { Text, View } from "../components/Themed";
+import { supabase } from "../config/supabase";
+import { RootTabScreenProps } from "../types";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+export default function TabOneScreen({
+  navigation,
+}: RootTabScreenProps<"TabOne">) {
+  const [user, setUser] = useState<any>(null);
+  const [messages, setMessages]: any = useState([]);
+  const [aichats, setAichats]: any = useState([]);
+  const [newChat, setNewChat] = useState("");
 
-export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
-  );
+  useEffect(() => {
+    //get user from supabase
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setUser(session?.user);
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user);
+    });
+  }, []);
+
+  useEffect(() => {
+    //get all chats from supabase
+    const getData = async () => {
+      if (!user) {
+        return;
+      }
+
+      const { data, error } = await supabase
+        .from("aichats")
+        .select("*")
+        .order("created_at", { ascending: true })
+        .eq("userid", user.id);
+      if (error) {
+        console.log("error", error);
+        Alert.alert("Error", error.message);
+      } else {
+        // Format the created_at value for each item
+        const formattedData = data.map((item: any) => {
+          const date = new Date(item.created_at);
+          const time = date.toLocaleTimeString();
+          const dateStr = date.toLocaleDateString();
+          const createdAt = dateStr + " " + time;
+          return {
+            ...item,
+            created_at: createdAt,
+          };
+        });
+        setAichats(formattedData);
+      }
+    };
+    getData();
+  }, [user]);
+
+  if (user) {
+    return (
+      <View style={styles.container}>
+        <ScrollView style={styles.scroll}>
+          {aichats &&
+            aichats.length > 0 &&
+            aichats.map((aichat: any, index: number) => (
+              <TouchableOpacity
+                key={index}
+                // onPress={() => navigation.navigate("AIChatScreen")}
+              >
+                <View style={styles.infoContainer}>
+                  <Image
+                    style={styles.avatar}
+                    source={{
+                      uri: "https://freesvg.org/img/1538298822.png",
+                    }}
+                  />
+                  <View style={styles.infoTextContainer}>
+                    <Text style={styles.infoTitle}>{aichat?.name}</Text>
+                    <Text style={styles.infoSubtitle}>
+                      {aichat?.created_at}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableOpacity>
+            ))}
+        </ScrollView>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
   },
-  title: {
+  headerContainer: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  icon: {
+    marginBottom: 10,
+  },
+  headerText: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  infoContainer: {
+    width: "90%",
+    padding: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    borderRadius: 10,
+    marginLeft: 20,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 20,
+  },
+  infoTextContainer: {
+    flex: 1,
+  },
+  infoTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  infoSubtitle: {
+    fontSize: 14,
+    color: "#666",
+  },
+  signOutButton: {
+    marginTop: 20,
+  },
+  addchatview: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+  infoTitleInput: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 16,
+  },
+  iconAddChat: {
+    position: "absolute",
+    right: 10,
+  },
+  headerContainerok: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  scroll: {
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
   },
 });
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,14 +1,177 @@
-import { StyleSheet } from 'react-native';
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  Linking,
+  ScrollView,
+  Share,
+  StyleSheet,
+  TextInput,
+  ToastAndroid,
+  TouchableOpacity,
+} from "react-native";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
+import { Text, View } from "../components/Themed";
+import { supabase } from "../config/supabase";
 
-export default function TabTwoScreen() {
+export default function TabTwoScreen({ navigation }: any) {
+  const [user, setUser] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [fullName, setFullName] = useState("");
+  const [error, setError] = useState("");
+  const [shared, setShared] = useState(false);
+  const rateUsOnAppStore = () => {
+    //play store
+    Linking.openURL(
+      "https://play.google.com/store/apps/details?id=com.ayushgupta.ai_chat"
+    );
+  };
+  const handleShareOnWatsapp = async () => {
+    //Watsapp share
+    try {
+      const result = await Share.share({
+        message:
+          "Hey there! I just discovered this amazing app that I think you'll love. It's packed with features that make [insert app purpose here] so much easier and more fun. Check it out for yourself and let me know what you think! #JustAiChat #JustAiChatApp Click here to download the app https://play.google.com/store/apps/details?id=com.ayushgupta.ai_chat",
+      });
+
+      if (result.action === Share.sharedAction) {
+        setShared(true);
+      }
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      if (session) {
+        setUser(session.user);
+      }
+    });
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setUser(session?.user);
+    });
+    //if user get full name and email
+    if (user) {
+      //get full name from database profile table
+      supabase
+        .from("profiles")
+        .select("full_name")
+        .eq("id", user.id)
+        .then(({ data, error }) => {
+          if (error) {
+            console.log("error", error);
+            Alert.alert("Error", error.message);
+          } else {
+            setFullName(data[0].full_name);
+          }
+        });
+    }
+  }, [user]);
+
+  const SignOut = async () => {
+    setLoading(true);
+    try {
+      const { error } = await supabase.auth.signOut();
+      if (error) throw setError(error.message);
+      setLoading(false);
+    } catch (error: any) {
+      console.log("error", error);
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+
+  const handlepass = async () => {
+    setLoading(true);
+    try {
+      if (!user) return;
+      const { data, error } = await supabase.auth.resetPasswordForEmail(
+        user.email
+      );
+      if (error) {
+        throw error;
+      }
+      console.log(data);
+      setLoading(false);
+      ToastAndroid.show("Password reset link sent to your email.", 1000);
+    } catch (error: any) {
+      setError(error.message);
+      ToastAndroid.show(error.message, 1000);
+      setLoading(false);
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      <Text style={styles.title}>Settings</Text>
+      <ScrollView style={styles.scroll}>
+        <TouchableOpacity>
+          <View style={styles.infoContainer}>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoTitle}>Name</Text>
+              <Text style={styles.infoSubtitle}>
+                {user ? fullName : "Loading..."}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <View style={styles.infoContainer}>
+            <View style={styles.infoTextContainer}>
+              <Text style={styles.infoTitle}>Email</Text>
+              <Text style={styles.infoSubtitle}>
+                {user ? user.email : "Loading..."}
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={rateUsOnAppStore}>
+          <View style={styles.infoContainerrateus}>
+            <View style={styles.infoTextContainerrateus}>
+              <Text style={styles.infoTitle}>
+                Click and Rate us & Shine bright.
+              </Text>
+              <Text style={styles.infoSubtitle}>
+                We've put a lot of hard work into creating this app, and it
+                would mean the world to us if you could take a moment to rate us
+                on the Play Store. Your support and feedback will help us
+                continue to improve and provide a truly fantastic experience.
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleShareOnWatsapp}>
+          <View style={styles.infoContainerrateus}>
+            <View style={styles.infoTextContainerrateus}>
+              <Text style={styles.infoTitle}>
+                Share this app with your friends and family.
+              </Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handlepass}>
+          <View style={styles.infoContainerlogoutres}>
+            <View style={styles.infoTextContainerlogout}>
+              <Text style={styles.infoTitle}>Reset Password</Text>
+              <Text style={styles.infoSubtitle}></Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={SignOut}>
+          <View style={styles.infoContainerlogout}>
+            <View style={styles.infoTextContainerlogout}>
+              <Text style={styles.infoTitle}>Logout</Text>
+              <Text style={styles.infoSubtitle}></Text>
+            </View>
+          </View>
+        </TouchableOpacity>
+      </ScrollView>
+      {error ? <Text style={styles.errorText}>{error}</Text> : null}
+      {loading ? (
+        <ActivityIndicator style={styles.loadingIndicator} size="large" />
+      ) : null}
     </View>
   );
 }
@@ -16,16 +179,166 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    backgroundColor: "#f5f5f5",
+  },
+  headerContainer: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  icon: {
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  infoContainer: {
+    width: "90%",
+    padding: 15,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    borderRadius: 10,
+    marginLeft: 20,
+    paddingLeft: 20,
+  },
+  infoContainerrateus: {
+    width: "90%",
+    padding: 15,
+    backgroundColor: "#00ffff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    borderRadius: 10,
+    marginLeft: 20,
+    paddingLeft: 20,
+  },
+  avatar: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    marginRight: 20,
+  },
+  infoTextContainer: {
+    flex: 1,
+  },
+  infoTextContainerrateus: {
+    flex: 1,
+    backgroundColor: "#00ffff",
+  },
+  infoTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  infoSubtitle: {
+    fontSize: 14,
+    color: "#666",
+  },
+  signOutButton: {
+    marginTop: 20,
+  },
+  addchatview: {
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "relative",
+  },
+  infoTitleInput: {
+    width: "100%",
+    height: 40,
+    borderColor: "#ddd",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    fontSize: 16,
+  },
+  iconAddChat: {
+    position: "absolute",
+    right: 10,
+  },
+  headerContainerok: {
+    width: "100%",
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+    alignItems: "center",
+    justifyContent: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
+  },
+  scroll: {
+    width: "100%",
+    backgroundColor: "#f5f5f5",
+    borderBottomWidth: 1,
+    borderBottomColor: "#ddd",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+    marginTop: 20,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+  errorText: {
+    color: "red",
+    marginTop: 20,
+  },
+  loadingIndicator: {
+    marginTop: 20,
+  },
+  infoTextContainerlogout: {
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  infoContainerlogout: {
+    width: "90%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    borderRadius: 10,
+    marginLeft: 20,
+    paddingLeft: 20,
+    paddingTop: 20,
+    borderWidth: 1,
+    borderColor: "#000",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    marginBottom: 20,
+  },
+  infoContainerlogoutres: {
+    width: "90%",
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    marginTop: 20,
+    borderRadius: 10,
+    marginLeft: 20,
+    paddingLeft: 20,
+    paddingTop: 20,
+    borderWidth: 1,
+    borderColor: "#000",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
   },
 });
