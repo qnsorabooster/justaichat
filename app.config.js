2530b3e7ca5c697b918c3e2494c41ee8b1b3f75e
qnsorabooster <124510198+qnsorabooster@users.noreply.github.com>
2023-02-22 14:24:07 +0000

    oko

--- app.config.js
+++ app.config.js
@@ -22,6 +22,7 @@ export default {
       supportsTablet: true,
     },
     android: {
+      package: "com.justaichat.justaichat",
       adaptiveIcon: {
         foregroundImage: "./assets/images/adaptive-icon.png",
         backgroundColor: "#ffffff",
@@ -31,6 +32,17 @@ export default {
       favicon: "./assets/images/favicon.png",
     },
     extra: {
+      firebaseApiKey: process.env.FIREBASE_API_KEY,
+      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: process.env.FIREBASE_APP_ID,
+      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID,
+      openAIKey: process.env.OPENAI_API_KEY,
+      openAIOrganization: process.env.OPENAI_ORG_ID,
+      supabaseurl: process.env.REACT_APP_SUPABASE_URL,
+      supabasekey: process.env.REACT_APP_SUPABASE_KEY,
       eas: {
         projectId: "44e0650c-7dc1-4588-a36e-f5c8ccc7c6c3",
       },
