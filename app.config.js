45e4681aaf9814e52798aef81a81c96fae73cf46
jaydeepdhrangiya-admin <recentpagetoday@gmail.com>
2023-02-28 21:35:41 +0800

    oko

--- app.config.js
+++ app.config.js
@@ -41,7 +41,6 @@ export default {
       supabaseurl: process.env.REACT_APP_SUPABASE_URL,
       supabasekey: process.env.REACT_APP_SUPABASE_KEY,
       stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-      stripeSecretKey: process.env.STRIPE_SECRET_KEY,
       eas: {
         projectId: "44e0650c-7dc1-4588-a36e-f5c8ccc7c6c3",
       },
